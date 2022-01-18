a97f27142905f644fe129655856f22bed62aebce
onoasu <64629201+onoasu@users.noreply.github.com>
2022-01-19 01:09:53 +0900

    Update 背景アニメーション追加

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,15 +3,32 @@ var __webpack_exports__ = {};
 /*!*************************!*\
   !*** ./src/js/index.js ***!
   \*************************/
-$(function() {
-  var pos = $("#contents").offset().top;
-  var height = $(".header").outerHeight();
+$(function () {
+  var pos = $('#contents').offset().top;
+
   $(window).scroll(function () {
-      if ($(this).scrollTop() > pos) {
-          $(".header").addClass("fix");
-      } else {
-          $(".header").removeClass("fix");
-      }
+    if ($(this).scrollTop() > pos) {
+      $('.header').addClass('fix');
+    } else {
+      $('.header').removeClass('fix');
+    }
+  });
+
+  var footer = $(".footer");
+  var fotterHeight = footer.offset().top;
+  var windowH = $(window).height();
+
+  $(window).scroll(function () {
+    var windowS = $(window).scrollTop();
+    if (windowS > fotterHeight - windowH + (windowH / 2) ) { // フッター付近まで来たら
+      $('.linearbg').css({
+        'opacity':'1',
+      });
+    } else {
+      $('.linearbg').css({
+        'opacity':'0',
+    });
+    }
   });
 });
 /******/ })()
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,28 @@
-$(function() {
-  var pos = $("#contents").offset().top;
-  var height = $(".header").outerHeight();
+$(function () {
+  var pos = $('#contents').offset().top;
+
   $(window).scroll(function () {
-      if ($(this).scrollTop() > pos) {
-          $(".header").addClass("fix");
-      } else {
-          $(".header").removeClass("fix");
-      }
+    if ($(this).scrollTop() > pos) {
+      $('.header').addClass('fix');
+    } else {
+      $('.header').removeClass('fix');
+    }
+  });
+
+  var footer = $(".footer");
+  var fotterHeight = footer.offset().top;
+  var windowH = $(window).height();
+
+  $(window).scroll(function () {
+    var windowS = $(window).scrollTop();
+    if (windowS > fotterHeight - windowH + (windowH / 2) ) { // フッター付近まで来たら
+      $('.linearbg').css({
+        'opacity':'1',
+      });
+    } else {
+      $('.linearbg').css({
+        'opacity':'0',
+    });
+    }
   });
 });
